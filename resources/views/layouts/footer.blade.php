a1b838eb089628bfa144d1d4916c2cbdc94bd659
aliansyahh <bb457640@gmail.com>
2021-03-26 20:11:26 +0700

    Intregasi laravel & admin lte

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,29 @@
+ <!-- Main Footer -->
+ <footer class="main-footer">
+     <strong>Copyright &copy; 2021 <a href="">Monitoring Mahasiswa</a>.</strong>
+     All rights reserved.
+     <div class="float-right d-none d-sm-inline-block">
+         <!-- <b>Version</b> 3.1.0 -->
+     </div>
+ </footer>
+ </div>
+ <!-- ./wrapper -->
+
+ <!-- REQUIRED SCRIPTS -->
+
+ <!-- jQuery -->
+ <script src="{{asset('/assets/admin')}}/plugins/jquery/jquery.min.js"></script>
+ <!-- Bootstrap -->
+ <script src="{{asset('/assets/admin')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <!-- AdminLTE -->
+ <script src="{{asset('/assets/admin')}}/dist/js/adminlte.js"></script>
+
+ <!-- OPTIONAL SCRIPTS -->
+ <script src="{{asset('/assets/admin')}}/plugins/chart.js/Chart.min.js"></script>
+ <!-- AdminLTE for demo purposes -->
+ <script src="{{asset('/assets/admin')}}/dist/js/demo.js"></script>
+ <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+ <script src="{{asset('/assets/admin')}}/dist/js/pages/dashboard3.js"></script>
+ </body>
+
+ </html>
